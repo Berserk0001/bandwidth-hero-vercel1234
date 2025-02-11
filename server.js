bee9d21f1948f41bbd5ec74b300cfcd2d3058e25
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-02-11 23:35:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@
 'use strict';
 
 import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
+
 import authenticate from './src/authenticate.js';
 import params from './src/params.js';
 import proxy from './src/proxy.js';
@@ -13,35 +11,15 @@ import proxy from './src/proxy.js';
 dotenv.config();
 
 const app = express();
-const PORT = parseInt(process.env.PORT, 10) || 443;
+const PORT =8080;
 
-// Security Middleware
-app.use(helmet.hidePoweredBy());
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
-app.use(helmet.frameguard({ action: 'deny' }));
-app.use(
-  helmet.contentSecurityPolicy({
-    useDefaults: true,
-    directives: {
-      defaultSrc: ["'self'"],
-      imgSrc: ["'self'", 'data:', 'https:'],
-    },
-  })
-);
 
-// HTTP request logging
-app.use(morgan('combined'));
 
 // Trust proxy for secure cookies and HTTPS redirection
 app.enable('trust proxy');
 
 // Routes
 app.get('/', authenticate, params, proxy);
-
-// Health check route
-app.get('/healthz', (req, res) => res.status(200).send('OK'));
 
 // Handle favicon requests
 app.get('/favicon.ico', (req, res) => res.status(204).end());
