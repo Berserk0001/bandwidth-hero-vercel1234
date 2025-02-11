7959724555e748d82544a05b62d5a9ba59dbd11a
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2025-02-11 21:59:52 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -12,16 +12,14 @@ function compress(req, res, input) {
       optimizeScans: true,
       effort:0
     })
-    .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) {
-        return redirect(req, res);
-      }
+    .toBuffer({ resolveWithObject: true })
+    .then(({ data, info }) => {
 
       res.setHeader('content-type', `image/${format}`);
       res.setHeader('content-length', info.size);
       res.setHeader('x-original-size', req.params.originSize);
       res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-      res.status(200).send(output);
+      res.status(200).send(data);
     });
 }
 
